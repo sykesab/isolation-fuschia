1be2e2dda3a278c80db2970e0a53accd865e884a
Aaron Sykes <absykes1997@gmail.com>
2018-11-18 09:34:58 -0500

    Added base player for our strategy

---
+++ Code/fuchsiaplayer.py
@@ -0,0 +1,11 @@
+import isolation
+import random
+
+
+class FuchsiaPlayer:
+
+    def __init__(self, name, token):
+        super.__init__(name, token)
+
+    def take_turn(self, board):
+        pass
